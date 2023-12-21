a720877b36b1c9ec25ced23b29a068af7398dece
EgorovAlexandr <egorov.dev@gmail.com>
2023-12-21 06:45:23 +0300

    удаление конкретной задачи

--- main.js
+++ main.js
@@ -1,31 +1,37 @@
-const inputText = document.querySelector('.header__input')
-const addBtn = document.querySelector('.header__button')
+const inputText = document.querySelector('.footer__input')
+const addBtn = document.querySelector('.footer__button')
 const todoList = document.querySelector('.content__list')
-const deleteBtn = document.querySelector('.header__remove')
+const deleteBtn = document.querySelector('.footer__remove')
 
-let todos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : []
-const renderTodo = () =>  {
+let todos = localStorage.getItem('todos')
+	? JSON.parse(localStorage.getItem('todos'))
+	: []
+const renderTodo = () => {
 	todoList.innerHTML = ''
 
-	if(todos.length <= 0) {
+	if (todos.length <= 0) {
 		const span = document.createElement('span')
 		span.textContent = 'Список дел пуст'
 		todoList.appendChild(span)
+		deleteBtn.classList.add('disabled')
 	}
 	todos.forEach(todo => {
 		const item = document.createElement('li')
 		const iconDel = document.createElement('div')
 		const icon = document.createElement('img')
-		icon.src = './image/delete.png'
+		icon.src = './image/dellItem.svg'
 		icon.alt = 'delete icon'
 		item.className = 'item'
-		iconDel.className = 'item-del'
+		icon.className = 'item-del'
 		item.textContent = todo.title
 		iconDel.appendChild(icon)
 		item.appendChild(iconDel)
 		todoList.appendChild(item)
 	})
 }
+
+renderTodo()
+
 const createTodo = () => {
 	const newTodo = {
 		id: new Date(),
@@ -43,12 +49,28 @@ const removeAllTodo = () => {
 	renderTodo()
 	window.location.reload()
 }
-const removeTodo = () => {
-	const currentTodo = todos.filter(todo => todo.id === id)
-	console.log(currentTodo)
+const removeTodo = e => {
+	// Выбор элемента внутри общего родителя
+	const targetItem = e.target
+	try {
+		// если элемент содержит класс item-del
+		if (targetItem.classList.contains('item-del')) {
+			// тут определяем конкретный item у которого был элемент с классом item-del
+			const listItem = targetItem.closest('.item')
+			// получаем индекс этого элемента
+			const index = Array.from(listItem.parentNode.children).indexOf(listItem)
+			// удаляем из массива
+			todos.splice(index, 1)
+			// обновляем локальное хранилище
+			localStorage.setItem('todos', JSON.stringify(todos))
+			// перерисовываем список дел
+			renderTodo()
+		}
+	} catch (error) {
+		console.warn(error)
+	}
 }
-
 addBtn.addEventListener('click', createTodo)
 deleteBtn.addEventListener('click', removeAllTodo)
 
-renderTodo()
+todoList.addEventListener('click', removeTodo)
